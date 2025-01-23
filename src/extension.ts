f1668a45ccae8e304e09ca25c9cd349e9224e11a
k1LoW <k1lowxb@gmail.com>
2025-01-23 13:57:50 +0900

    feat: Add cursorReturn

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	let beforeLine = 0;
+	let beforeCharacter = 0;
+
 	const watcher = vscode.window.onDidChangeTextEditorSelection((event) => {
 		const editor = event.textEditor;
 		const selection = editor.selection;
@@ -14,6 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
 		const cursorEnd = (character === lineLength);
 		const cursorBottom = (line === lastLine);
 
+		if (!cursorTop && !cursorBottom) {
+			beforeLine = line;
+		}
+		if (!cursorHome && !cursorEnd) {
+			beforeCharacter = character;
+		}
+
 		const config = vscode.workspace.getConfiguration('cursor-position-contexts');
 		config.update('cursorTop', cursorTop);
 		config.update('cursorHome', cursorHome);
@@ -23,17 +33,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(watcher);
 
-	const disposable = vscode.commands.registerCommand('cursor-position-contexts.showContexts', () => {
+	const cursorReturn = vscode.commands.registerCommand('cursor-position-contexts.cursorReturn', () => {
+		const editor = vscode.window.activeTextEditor;
+		const newPosition = new vscode.Position(beforeLine, beforeCharacter);
+		const newSelection = new vscode.Selection(newPosition, newPosition);
+		if (editor) {
+			editor.selection = newSelection;
+			editor.revealRange(newSelection);
+		}
+	});
+
+	context.subscriptions.push(cursorReturn);
+
+	const showContexts = vscode.commands.registerCommand('cursor-position-contexts.showContexts', () => {
 		const config = vscode.workspace.getConfiguration('cursor-position-contexts');
 		const cursorTop = config.get('cursorTop');
 		const cursorHome = config.get('cursorHome');
 		const cursorEnd = config.get('cursorEnd');
 		const cursorBottom = config.get('cursorBottom');
 
-		vscode.window.showInformationMessage(`cursorTop: ${cursorTop}, cursorHome: ${cursorHome}, cursorEnd: ${cursorEnd}, cursorBottom: ${cursorBottom}`);
+		vscode.window.showInformationMessage(`!!cursorTop: ${cursorTop}, cursorHome: ${cursorHome}, cursorEnd: ${cursorEnd}, cursorBottom: ${cursorBottom}`);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(showContexts);
 }
 
 export function deactivate() { }
